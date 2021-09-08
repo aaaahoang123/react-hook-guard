9403d5a52f64af261b67692c68f5afd5c9d2f556
Hoang Do <hoangdo.2194@gmail.com>
2021-09-08 21:30:56 +0700

    - Apply test.

--- src/__tests__/utils/resolveRoute.test.ts
+++ src/__tests__/utils/resolveRoute.test.ts
@@ -1,3 +1,88 @@
-test('Resolve simple route', () => {
+import {Route, RouteWithCommand} from '../../define';
+import {resolveRoute} from '../../utils';
 
+test('Test resolve absolute without parent', () => {
+    const absoluteRoute: Route = {
+        path: '/',
+    };
+
+    expect(resolveRoute(absoluteRoute))
+        .toEqual({
+            path: '/',
+            absolutePath: '/',
+        });
+});
+
+test('Test resolve relative route without parent', () => {
+    const relativeRoute: Route = {
+        path: '',
+    };
+
+    expect(resolveRoute(relativeRoute, undefined, true))
+        .toEqual({
+            path: '',
+            absolutePath: '/',
+            commands: [],
+        });
+});
+
+test('Test resolve absolute route with parent', () => {
+    const parentRoute: Route = {
+        path: '/',
+    };
+
+    const absoluteRoute: Route = {
+        path: '/',
+    };
+
+    expect(resolveRoute(absoluteRoute, parentRoute))
+        .toEqual({
+            path: '/',
+            absolutePath: '/',
+            canActivate: [],
+        });
+});
+
+test('Test resolve relative route with parent', () => {
+    const parentRoute: RouteWithCommand = {
+        path: '',
+        commands: [],
+        absolutePath: '/'
+    };
+
+    const relativeRoute: Route = {
+        path: '',
+    };
+
+    expect(resolveRoute(relativeRoute, parentRoute, true))
+        .toEqual({
+            path: '',
+            commands: [],
+            absolutePath: '/',
+            canActivate: [],
+        });
+});
+
+
+const useTestCanActivate = () => {
+    return true;
+};
+
+test('Test copy canActivateChild from parent', () => {
+    const parentRoute: RouteWithCommand = {
+        path: '',
+        canActivateChild: [useTestCanActivate],
+    };
+
+    const route: Route = {
+        path: ''
+    };
+
+    expect(resolveRoute(route, parentRoute, true))
+        .toEqual({
+            path: '',
+            absolutePath: '/',
+            commands: [],
+            canActivate: [useTestCanActivate],
+        })
 });
