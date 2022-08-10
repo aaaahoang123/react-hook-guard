edfdaf16be5d2e4afe668933aa57bffa6164cec1
hoangdo <hoangdo@topcv.vn>
2022-08-10 11:35:21 +0700

    Chore: Update dependencies.

--- example/src/guards/useAuthorization.ts
+++ example/src/guards/useAuthorization.ts
@@ -2,7 +2,7 @@ import {useAuth} from './AuthProvider';
 import {Route} from 'react-hook-guard';
 function useAuthorization(route: Route) {
     const auth = useAuth();
-    return !route.data?.role || auth.roles.includes(route.data.role);
+    return !route?.data?.role || auth.roles.includes(route?.data.role);
 }
 
 export default useAuthorization;
